ffb40ce00f0ac3e4db2da432501ee26ae139bf20
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-06-22 15:55:32 +0700

    element yang di hapus berada di posisi terakhir array, kembali ke awal array

--- circularQueues_115/circularQueues_115.cpp
+++ circularQueues_115/circularQueues_115.cpp
@@ -51,3 +51,11 @@ public:
 			FRONT = -1;
 			REAR = -1;
 		}
+		else {
+			//jika element yang di hapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
